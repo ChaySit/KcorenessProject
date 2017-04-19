2c71d335f2b29a03567858817d9f515935c2d905
khadijafen9 <fennine.khadija@gmail.com>
2017-04-19 23:12:02 +0200

    Update KcorenessInitializer.java

--- src/projet/Kcoreness/KcorenessInitializer.java
+++ src/projet/Kcoreness/KcorenessInitializer.java
@@ -1,5 +1,41 @@
 package projet.Kcoreness;
 
-public class KcorenessInitializer {
+import peersim.config.Configuration;
+import peersim.core.Control;
+import peersim.core.Linkable;
+import peersim.core.Network;
+import peersim.core.Node;
+
+
+public class KcorenessInitializer implements Control {
+  
+   //Parameters
+    private static final String PAR_PROT = "protocol";
+    private static final String LINKABLE_PROT = "linkable";
+
+    //Fields
+    private static int pid;
+    private static int linkpid;
+
+    //Constructor
+    public KcorenessInitializer(String prefix){
+        pid = Configuration.getPid(prefix + "."+PAR_PROT);
+        linkpid = Configuration.getPid(prefix + "." + LINKABLE_PROT);
+    }
+
+    @Override
+    public boolean execute() {
+        for (int i=0; i< Network.size(); i++){
+            CorenessProtocol protocol = (CorenessProtocol) Network.get(i).getProtocol(pid);
+            Linkable linkable = (Linkable) Network.get(i).getProtocol(linkpid);
+            protocol.setChanged(false);
+            protocol.setCoreness(linkable.degree());
+            for (int j=0; j<linkable.degree(); j++){
+                Node peer = linkable.getNeighbor(j);
+                protocol.newEntry(peer);
+            }
+        }
+        return false;
+    }
 
 }
