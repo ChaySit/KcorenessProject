a01617e23afce16638fe7e1e5f98ace50309ff89
Mohammed Elkhadiri <mohammed.el_khadiri@insa-cvl.fr>
2017-05-12 20:08:06 +0200

    modification classe KcorenessImporter

--- src/projet/Kcoreness/KcorenessImporter.java
+++ src/projet/Kcoreness/KcorenessImporter.java
@@ -1,18 +1,31 @@
 package projet.Kcoreness;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 import peersim.config.Configuration;
 import peersim.core.Control;
 import peersim.core.Linkable;
 import peersim.core.Network;
 import peersim.core.Node;
 
-
+/**
+ * load graph from graphML file
+ */
 public class KcorenessImporter implements Control {
     
 
     //Parameters
     private static final String PAR_PROT = "protocol";
     private static final String LINKABLE_PROT = "linkable";
+    public static final String GRAPHML_READING_PATH = "D:\\Workspace\\peerSim\\graphs\\graph0.graphml";
+
 
     //Fields
     private static int pid;
@@ -26,19 +39,41 @@ public class KcorenessImporter implements Control {
 
     @Override
     public boolean execute() {
-    	//Need modification
-        for (int i=0; i< Network.size(); i++){
-        	KcorenessFunction protocol = (KcorenessFunction) Network.get(i).getProtocol(pid);
-            Linkable linkable = (Linkable) Network.get(i).getProtocol(linkpid);
-            protocol.setChanged(false);
-            protocol.setCoreness(linkable.degree());
-            for (int j=0; j<linkable.degree(); j++){
-                Node peer = linkable.getNeighbor(j);
-                protocol.newEntry(peer);
-            }
-        }
-        
-        
+    	
+    	try {    
+		    File fXmlFile = new File(GRAPHML_READING_PATH);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            
+            doc.getDocumentElement().normalize();   
+            NodeList edgeList = doc.getElementsByTagName("edge");  
+            
+           for (int i = 0; i < edgeList.getLength(); i++) {
+        	   org.w3c.dom.Node edge = edgeList.item(i);
+               if (edge.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {  
+				    Element eElement = (Element) edge;
+                   int s = Integer.parseInt(eElement.getAttribute("source"));
+                   int t = Integer.parseInt(eElement.getAttribute("target"));
+                   
+                   /*
+                    * TODO create nodes/edges
+                   KcorenessFunction protocol = (KcorenessFunction) Network.get(i).getProtocol(pid);
+                   Linkable linkable = (Linkable) Network.get(i).getProtocol(linkpid);
+                   protocol.setChanged(false);
+                   protocol.setCoreness(linkable.degree());
+                   for (int j=0; j<linkable.degree(); j++){
+                       Node peer = linkable.getNeighbor(j);
+                       protocol.newEntry(peer);
+                     
+                   }*/
+               }
+           }
+
+       } catch (Exception e) {
+               e.printStackTrace();
+       }	
+                
         return false;
     }
 }
